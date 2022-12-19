336d1482e3f3321f36d1f655771a82906e79086e
KrishenK <fox2581kriskenk@gmail.com>
2022-12-19 20:53:34 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -17,39 +17,45 @@ with open('README.md') as readme_file:
 with open('requirements.txt') as requirements_file:
     requirements = requirements_file.read().strip().split()
 
-setup(
-    name='anicli',
-    version=__version__,
-    description="CLI to browse and watch anime in vostfr (inspirated by pystardust's ani-cli)",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author='KrishenK',
-    url='https://github.com/KrishenK0/ani-cli',
-    install_requires=requirements,
-    include_package_data=True,
-    packages=find_packages(),
-    package_data={'': ['mpv-2.dll']},
-    package_dir={'anicli':'anicli'}, 
-    license='MIT',
-    zip_safe=False,
-    keywords='ani-cli vostfr',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    # test_suite='tests',
-    entry_points={
-        'console_scripts': [
-            'ani-cli=anicli.anicli:main',
+try:
+    setup(
+        name='anicli',
+        version=__version__,
+        description="CLI to browse and watch anime in vostfr (inspirated by pystardust's ani-cli)",
+        python_requires='<=3.10.6, >=3, ~=3.10.6',
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        author='KrishenK',
+        url='https://github.com/KrishenK0/ani-cli',
+        install_requires=requirements,
+        include_package_data=True,
+        packages=find_packages(),
+        package_data={'': ['mpv-2.dll']},
+        package_dir={'anicli':'anicli'},
+        license='MIT',
+        zip_safe=False,
+        keywords='anicli vostfr',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
-    },
-)
+        # test_suite='tests',
+        entry_points={
+            'console_scripts': [
+                'ani-cli=anicli.anicli:main',
+            ],
+        },
+    )
+except e:
+    print(f"Error : {e}\nRelease tested version : 3.10.6 (Try this version)")
+    pass
